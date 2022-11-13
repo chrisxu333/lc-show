dba1b1a2919aed410077a3192813def1c5d581aa
chrisxu333 <xu.3296@osu.edu>
2022-11-14 04:30:05 +0800

    test II

--- api/index.py
+++ api/index.py
@@ -9,8 +9,11 @@ def getTodayRecord():
         response = requests.post(base_url + "/graphql", json={
             "query": "\n    query questionOfToday {\n  todayRecord {\n    date\n    userStatus\n    question {\n      questionId\n      frontendQuestionId: questionFrontendId\n      difficulty\n      title\n      titleCn: translatedTitle\n      titleSlug\n      paidOnly: isPaidOnly\n      freqBar\n      isFavor\n      acRate\n      status\n      solutionNum\n      hasVideoSolution\n      topicTags {\n        name\n        nameTranslated: translatedName\n        id\n      }\n      extra {\n        topCompanyTags {\n          imgUrl\n          slug\n          numSubscribed\n        }\n      }\n    }\n    lastSubmission {\n      id\n    }\n  }\n}\n    "
         })
-        leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get("question").get('title')
-        return leetcodeTitle
+        data = json.loads(response.text).get('data')
+        title = data.get('todayRecord')[0].get("question").get('title')
+        difficulty = data.get('todayRecord')[0].get("question").get('difficulty')
+        acRate = data.get('todayRecord')[0].get("question").get('acRate')
+        return title, difficulty, acRate
     except requests.exceptions.RequestException as e:
         raise SystemExit(e) 
  
@@ -21,8 +24,9 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         try:
-            todayRecord = getTodayRecord()
-            self.wfile.write(todayRecord.encode())
+            todayRecordName, difficulty, acRate = getTodayRecord()
+            displaystr = "Problem: " + todayRecordName + "\nDifficulty: " + difficulty + "\nAccept Rate: " + str(acRate)
+            self.wfile.write(displaystr.encode())
         except requests.exceptions.RequestException as e:
             self.wfile.write("failure".encode())        
         return
