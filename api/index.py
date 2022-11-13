09e36b61a008fa69fdf383e6e7f68750b489bb8f
chrisxu333 <xu.3296@osu.edu>
2022-11-14 03:55:33 +0800

    test today record api

--- api/index.py
+++ api/index.py
@@ -1,4 +1,16 @@
 from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+import requests
+import json
+import smtplib
+from email.mime.text import MIMEText
+
+def getTodayRecord():
+    response = requests.post(base_url + "/graphql", json={
+        "query": "\n    query questionOfToday {\n  todayRecord {\n    date\n    userStatus\n    question {\n      questionId\n      frontendQuestionId: questionFrontendId\n      difficulty\n      title\n      titleCn: translatedTitle\n      titleSlug\n      paidOnly: isPaidOnly\n      freqBar\n      isFavor\n      acRate\n      status\n      solutionNum\n      hasVideoSolution\n      topicTags {\n        name\n        nameTranslated: translatedName\n        id\n      }\n      extra {\n        topCompanyTags {\n          imgUrl\n          slug\n          numSubscribed\n        }\n      }\n    }\n    lastSubmission {\n      id\n    }\n  }\n}\n    "
+    })
+    leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get("question").get('titleCn')
+    return leetcodeTitle
  
  
 class handler(BaseHTTPRequestHandler):
@@ -7,5 +19,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write("Nuo Xu's API by ccw!".encode())
+        todayRecord = getTodayRecord()
+        self.wfile.write(todayRecord.encode())
+        
         return
