68db3b1bd7400c1af07574b6e8845c7921c16914
chrisxu333 <xu.3296@osu.edu>
2022-11-14 04:07:46 +0800

    test

--- api/index.py
+++ api/index.py
@@ -8,12 +8,14 @@ from email.mime.text import MIMEText
 base_url = 'https://leetcode-cn.com'
 
 def getTodayRecord():
-    response = requests.post(base_url + "/graphql", json={
-        "query": "\n    query questionOfToday {\n  todayRecord {\n    date\n    userStatus\n    question {\n      questionId\n      frontendQuestionId: questionFrontendId\n      difficulty\n      title\n      titleCn: translatedTitle\n      titleSlug\n      paidOnly: isPaidOnly\n      freqBar\n      isFavor\n      acRate\n      status\n      solutionNum\n      hasVideoSolution\n      topicTags {\n        name\n        nameTranslated: translatedName\n        id\n      }\n      extra {\n        topCompanyTags {\n          imgUrl\n          slug\n          numSubscribed\n        }\n      }\n    }\n    lastSubmission {\n      id\n    }\n  }\n}\n    "
-    })
-    leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get("question").get('titleCn')
-    return leetcodeTitle
- 
+    try:
+        response = requests.post(base_url + "/graphql", json={
+            "query": "\n    query questionOfToday {\n  todayRecord {\n    date\n    userStatus\n    question {\n      questionId\n      frontendQuestionId: questionFrontendId\n      difficulty\n      title\n      titleCn: translatedTitle\n      titleSlug\n      paidOnly: isPaidOnly\n      freqBar\n      isFavor\n      acRate\n      status\n      solutionNum\n      hasVideoSolution\n      topicTags {\n        name\n        nameTranslated: translatedName\n        id\n      }\n      extra {\n        topCompanyTags {\n          imgUrl\n          slug\n          numSubscribed\n        }\n      }\n    }\n    lastSubmission {\n      id\n    }\n  }\n}\n    "
+        })
+        leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get("question").get('titleCn')
+        return leetcodeTitle
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e) 
  
 class handler(BaseHTTPRequestHandler):
  
