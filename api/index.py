c0f6f7c04d01e127cdee52889c6e5ede7c13a5f1
chrisxu333 <xu.3296@osu.edu>
2022-11-15 06:48:32 +0800

    test

--- api/index.py
+++ api/index.py
@@ -8,37 +8,6 @@ session = requests.Session()
 user_agent = r'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
 base_url = 'https://leetcode-cn.com'
 
-# def login(username, pwd):
-#     login_url = base_url + '/accounts/login/'
-#     cookies = session.get(login_url).cookies
-#     for cookie in cookies:
-#         if cookie.name == 'csrftoken':
-#             csrftoken = cookie.value
-    
-#     params_data = {
-#         'csrfmiddlewaretoken': csrftoken,
-#         'login': username,
-#         'password':pwd,
-#         'next': '/problems'
-#     }
-#     headers = {'User-Agent': user_agent, 'Connection': 'keep-alive', 'Referer': login_url, "origin": base_url}
-#     m = MultipartEncoder(params_data)   
-
-#     headers['Content-Type'] = m.content_type
-#     session.post(login_url, headers = headers, data = m, timeout = 10, allow_redirects = False)
-#     is_login = session.cookies.get('LEETCODE_SESSION') != None
-#     return is_login
-
-def login():
-    login_url = 'https://leetcode.cn/accounts/login/'
-    print('Login to leetcode...')
-
-    session = requests.Session()
-    session.get(login_url)
-    cookie = session.cookies
-    a = cookie.get_dict()
-    return a
-
 def getTodayRecord():
     try:
         response = requests.post(base_url + "/graphql", json={
@@ -51,24 +20,29 @@ def getTodayRecord():
         return title, difficulty, acRate
     except requests.exceptions.RequestException as e:
         raise SystemExit(e) 
+
+def getPersonalProgress(username):
+    response = requests.post(base_url + "/graphql", json={
+        "query":"\n    query userQuestionProgress($userSlug: String!) {\n  userProfileUserQuestionProgress(userSlug: $userSlug) {\n    numAcceptedQuestions {\n      difficulty\n      count\n    }\n    numFailedQuestions {\n      difficulty\n      count\n    }\n    numUntouchedQuestions {\n      difficulty\n      count\n    }\n  }\n}\n    ","variables":{"userSlug":username}
+    })
+    data = json.loads(response.text).get('data')
+    easy_count = data.get('userProfileUserQuestionProgress').get("numAcceptedQuestions")[0].get('count')
+    return easy_count
  
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
         path = path.replace("'", '"')
         user_reg = re.compile(r'user="(.*?)"')
-        pwd_reg = re.compile(r'pwd="(.*?)"')
         user = user_reg.findall(path)[0]
-        pwd = pwd_reg.findall(path)[0]
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
-        # login section
-        token = login()
         try:
-            todayRecordName, difficulty, acRate = getTodayRecord()
-            displaystr = "Token: " + str(token) + "\nProblem: " + todayRecordName + "\nDifficulty: " + difficulty + "\nAccept Rate: " + str(acRate)
+            # todayRecordName, difficulty, acRate = getTodayRecord()
+            # displaystr = "Problem: " + todayRecordName + "\nDifficulty: " + difficulty + "\nAccept Rate: " + str(acRate)
+            displaystr = getPersonalProgress(user)
             self.wfile.write(displaystr.encode())
         except requests.exceptions.RequestException as e:
             self.wfile.write("failure".encode())        
