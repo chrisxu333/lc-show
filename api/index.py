a3de9456548bb8d4872938cc256ffefef7cc5bac
chrisxu333 <xu.3296@osu.edu>
2022-11-14 08:38:03 +0800

    add login

--- api/index.py
+++ api/index.py
@@ -2,8 +2,32 @@ from http.server import BaseHTTPRequestHandler
 import requests
 import re
 import json
+from requests_toolbelt import MultipartEncoder
 
+session = requests.Session()
+user_agent = r'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
 base_url = 'https://leetcode-cn.com'
+
+def login(username, pwd):
+    login_url = base_url + '/accounts/login/'
+    cookies = session.get(login_url).cookies
+    for cookie in cookies:
+        if cookie.name == 'csrftoken':
+            csrftoken = cookie.value
+    
+    params_data = {
+        'csrfmiddlewaretoken': csrftoken,
+        'login': username,
+        'password':pwd,
+        'next': '/problems'
+    }
+    headers = {'User-Agent': user_agent, 'Connection': 'keep-alive', 'Referer': login_url, "origin": base_url}
+    m = MultipartEncoder(params_data)   
+
+    headers['Content-Type'] = m.content_type
+    session.post(login_url, headers = headers, data = m, timeout = 10, allow_redirects = False)
+    is_login = session.cookies.get('LEETCODE_SESSION') != None
+    return is_login
 
 def getTodayRecord():
     try:
@@ -23,13 +47,15 @@ class handler(BaseHTTPRequestHandler):
         path = self.path
         path = path.replace("'", '"')
         user_reg = re.compile(r'user="(.*?)"')
+        pwd_reg = re.compile(r'pwd="(.*?)"')
         user = user_reg.findall(path)[0]
+        pwd = pwd_reg.findall(path)[0]
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         try:
             todayRecordName, difficulty, acRate = getTodayRecord()
-            displaystr = "User: " + user + "\nProblem: " + todayRecordName + "\nDifficulty: " + difficulty + "\nAccept Rate: " + str(acRate)
+            displaystr = "User: " + user + "pwd:" + pwd + "\nProblem: " + todayRecordName + "\nDifficulty: " + difficulty + "\nAccept Rate: " + str(acRate)
             self.wfile.write(displaystr.encode())
         except requests.exceptions.RequestException as e:
             self.wfile.write("failure".encode())        
